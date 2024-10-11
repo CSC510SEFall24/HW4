d18bc7f9c9eb3d861bcc224a44525e5af2200332
bhavishya <bhavishya2612@gmail.com>
2024-10-10 23:04:41 -0400

    Task2 redone

--- task2.sh
+++ task2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd dataset1
-for file in *; do
 
+for file in *; do
   if grep -q "sample" "$file" && [[ $(grep -o "sample" "$file" | wc -l) -eq 1 ]] && [[ $(grep -o "CSC510" "$file" | wc -l) -ge 3 ]]; then
-   
     echo "$(stat -f%z "$file") $(grep -o "CSC510" "$file" | wc -l) $file"
   fi
 done | sort -k2,2nr -k1,1n | sed 's/file_/filtered_/g'
